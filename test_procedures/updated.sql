6e53ecbc8de9ffdefa9276e0fd6ba69d5839f49c
Ishwar Jangid <41536582+ishwar6@users.noreply.github.com>
2023-04-14 19:57:28 +0530

    Create updated.sql

---
+++ test_procedures/updated.sql
@@ -0,0 +1,35 @@
+CREATE OR REPLACE PROCEDURE TEST(FULL_TABLE_NAME VARCHAR(16777216))
+RETURNS VARIANT
+LANGUAGE JAVASCRIPT
+EXECUTE AS CALLER
+AS '
+    var table_params;
+
+    if (FULL_TABLE_NAME.includes(".")) {
+        table_params = FULL_TABLE_NAME.split(".");
+        if (table_params.length != 2) {
+            return {error: "Table name does not have a schema."};
+        }
+        var schema_name = table_params[0];
+        table_params = table_params[1];
+    }
+    else {
+        table_params = FULL_TABLE_NAME;
+        var schema_name = "";
+    }
+
+    var params = table_params.match(/"(\d*?)__(\d*?)__([A-Za-z0-9_]*)__([A-Za-z0-9_]*)__([A-Za-z0-9_]*)"/);
+
+    if (params == null) {
+        params = table_params.match(/(\d*?)__(\d*?)__([A-Za-z0-9_]*)__([A-Za-z0-9_]*)__([A-Za-z0-9_]*)/);
+    }
+
+    if (params == null || params.length < 5) {
+        return {error: "Table name does not have enough parameters."};
+    }
+    
+
+    var array = params.slice(2,5);
+
+    return [schema_name].concat(array.map(col => col.toLowerCase()));
+';
